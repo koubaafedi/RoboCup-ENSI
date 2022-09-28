ce6c7201f1fbc55298a75f799163458ca0003ff5
ayachaieb <ayachaieb7@gmail.com>
2022-09-28 15:43:36 +0100

    zone3

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,9 @@
-// Black Line Follower 
-const int TopSensor=13;    //Right sensor
-const int LeftSensor=12;    //left Sensor
-const int RightSensor=11; //Right Sensor
-const int MiddleRightSensor=2; //Middle Right Sensor
-const int MiddleLeftSensor=3; //Middle Left Sensor
-
+// Black Line Follower
+const int TopSensor = 13; //Right sensor
+const int LeftSensor = 12; //left Sensor
+const int RightSensor = 11; //Right Sensor
+const int MiddleRightSensor = 2; //Middle Right Sensor
+const int MiddleLeftSensor = 3; //Middle Left Sensor
 //Moteur Droite
 const int MoteurDroiteForword = 7;
 const int MoteurDroiteBackword = 6;
@@ -13,7 +12,18 @@ const int MoteurDroiteSpeed = 9;
 const int MoteurGaucheForword = 5;
 const int MoteurGaucheBackword = 4;
 const int MoteurGaucheSpeed = 10;
+//VARIABLES
 
+typedef enum
+{
+  ROBOT_Etat0,
+  ROBOT_ModeSuiveur,
+  ROBOT_RouteSemiReduite,
+  ROBOT_RouteAutonome,
+  ROBOT_LadderMode,
+  ROBOT_Cercles,
+} Robot_States;
+Robot_States ROBOT_CurrentState_en = ROBOT_Etat0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,7 +33,7 @@ void setup() {
   pinMode(LeftSensor, INPUT);
   pinMode(RightSensor, INPUT);
   pinMode(MiddleRightSensor, INPUT);
-  pinMode(MiddleLeftSensor, INPUT);  
+  pinMode(MiddleLeftSensor, INPUT);
   //Motors
   pinMode(MoteurDroiteForword, OUTPUT);
   pinMode(MoteurDroiteBackword, OUTPUT);
@@ -33,126 +43,137 @@ void setup() {
   pinMode(MoteurGaucheSpeed, OUTPUT);
 }
 
-
-void loop() 
-{
-   if(digitalRead(TopSensor)==HIGH && digitalRead(MiddleRightSensor)==HIGH && digitalRead(MiddleLeftSensor)==HIGH )
+void loop() {
+  switch(ROBOT_CurrentState_en)
   {
-    GoForward();
+case ROBOT_Etat0:
+    { int start = millis();
+      GoForward();
+      if (millis() - start > 1000)
+        ROBOT_CurrentState_en = ROBOT_ModeSuiveur ;
+    } break;
+
+case ROBOT_ModeSuiveur:
+    {
+      LineFollower();
+      if (VerifTurnLeft())
+      {
+        TurnLeft();
+        delay(500);
+        ROBOT_CurrentState_en = ROBOT_ModeSuiveur;
+      }
+      
+    } break;
+
+Default:
+    {
+    }
   }
-  else if(digitalRead(LeftSensor)==HIGH && digitalRead(RightSensor)==LOW){
-    TurnLeft();
+}
+  // sensor function
+  bool VerifTurnLeft() {
+    return digitalRead(TopSensor) == LOW && digitalRead(MiddleRightSensor) == HIGH && digitalRead(LeftSensor) == HIGH && digitalRead(MiddleLeftSensor) == HIGH;
   }
-  else if(digitalRead(MiddleRightSensor)==LOW && digitalRead(MiddleLeftSensor)==HIGH ) 
+  //FONCTIONS MOTEURS
+
+  void GoForward()
   {
-    SlightlyTurnLeft();
+    //Controlling speed (0 = off and 255 = max speed):
+    analogWrite(MoteurDroiteSpeed, 100); //ENA pin
+    analogWrite(MoteurGaucheSpeed, 100); //ENB pin
+    //Controlling spin direction of motors:
+    digitalWrite(MoteurDroiteForword, HIGH);
+    digitalWrite(MoteurDroiteBackword, LOW);
+    digitalWrite(MoteurGaucheForword, HIGH);
+    digitalWrite(MoteurGaucheBackword, LOW);
   }
-  else if(digitalRead(MiddleRightSensor)==HIGH && digitalRead(MiddleLeftSensor)==LOW ) 
+  void SlightlyTurnLeft()
   {
-    SlightlyTurnRight();
+    //Controlling speed (0 = off and 255 = max speed):
+    analogWrite(MoteurDroiteSpeed, 100); //ENA pin
+    analogWrite(MoteurGaucheSpeed, 75); //ENB pin
+    //Controlling spin direction of motors:
+    digitalWrite(MoteurDroiteForword, HIGH);
+    digitalWrite(MoteurDroiteBackword, LOW);
+    digitalWrite(MoteurGaucheForword, LOW);
+    digitalWrite(MoteurGaucheBackword, HIGH);
   }
-  else if(digitalRead(LeftSensor)==LOW && digitalRead(RightSensor)==HIGH){
-    TurnRight();
+  void TurnLeft() {
+    //Controlling speed (0 = off and 255 = max speed):
+    analogWrite(MoteurDroiteSpeed, 100); //ENA pin
+    analogWrite(MoteurGaucheSpeed, 100); //ENB pin
+    //Controlling spin direction of motors:
+    digitalWrite(MoteurDroiteForword, HIGH);
+    digitalWrite(MoteurDroiteBackword, LOW);
+    digitalWrite(MoteurGaucheForword, LOW);
+    digitalWrite(MoteurGaucheBackword, HIGH);
   }
-  else if(digitalRead(MiddleRightSensor)==LOW && digitalRead(MiddleLeftSensor)==LOW ) 
+  void SlightlyTurnRight()
   {
-    STOP();
+    //Controlling speed (0 = off and 255 = max speed):
+    analogWrite(MoteurDroiteSpeed, 75); //ENA pin
+    analogWrite(MoteurGaucheSpeed, 100); //ENB pin
+    //Controlling spin direction of motors:
+    digitalWrite(MoteurDroiteForword, LOW);
+    digitalWrite(MoteurDroiteBackword, HIGH);
+    digitalWrite(MoteurGaucheForword, HIGH);
+    digitalWrite(MoteurGaucheBackword, LOW);
   }
-}
-
-//FONCTIONS MOTEURS
-void GoForward()
-{
- 
-  //Controlling speed (0 = off and 255 = max speed):
-  analogWrite(MoteurDroiteSpeed, 100); //ENA pin
-  analogWrite(MoteurGaucheSpeed, 100); //ENB pin
-
-  //Controlling spin direction of motors:
-  digitalWrite(MoteurDroiteForword, HIGH);
-  digitalWrite(MoteurDroiteBackword, LOW);
-
-  digitalWrite(MoteurGaucheForword, HIGH);
-  digitalWrite(MoteurGaucheBackword, LOW);
- 
-}
-
-
-void SlightlyTurnLeft()
-{ 
-  //Controlling speed (0 = off and 255 = max speed):
-  analogWrite(MoteurDroiteSpeed, 100); //ENA pin
-  analogWrite(MoteurGaucheSpeed, 75); //ENB pin
-
-  //Controlling spin direction of motors:
-  digitalWrite(MoteurDroiteForword, HIGH);
-  digitalWrite(MoteurDroiteBackword, LOW);
-
-  digitalWrite(MoteurGaucheForword, LOW);
-  digitalWrite(MoteurGaucheBackword, HIGH);
-}
-void TurnLeft(){
-  //Controlling speed (0 = off and 255 = max speed):
-  analogWrite(MoteurDroiteSpeed, 100); //ENA pin
-  analogWrite(MoteurGaucheSpeed, 100); //ENB pin
-
-  //Controlling spin direction of motors:
-  digitalWrite(MoteurDroiteForword, HIGH);
-  digitalWrite(MoteurDroiteBackword, LOW);
-
-  digitalWrite(MoteurGaucheForword, LOW);
-  digitalWrite(MoteurGaucheBackword, HIGH);
-}
-void SlightlyTurnRight()
-{
- //Controlling speed (0 = off and 255 = max speed):
-  analogWrite(MoteurDroiteSpeed, 75); //ENA pin
-  analogWrite(MoteurGaucheSpeed, 100); //ENB pin
-
-  //Controlling spin direction of motors:
-  digitalWrite(MoteurDroiteForword, LOW);
-  digitalWrite(MoteurDroiteBackword, HIGH);
-
-  digitalWrite(MoteurGaucheForword, HIGH);
-  digitalWrite(MoteurGaucheBackword, LOW);
-}
-void TurnRight()
-{
- //Controlling speed (0 = off and 255 = max speed):
-  analogWrite(MoteurDroiteSpeed, 100); //ENA pin
-  analogWrite(MoteurGaucheSpeed, 100); //ENB pin
-
-  //Controlling spin direction of motors:
-  digitalWrite(MoteurDroiteForword, LOW);
-  digitalWrite(MoteurDroiteBackword, HIGH);
-
-  digitalWrite(MoteurGaucheForword, HIGH);
-  digitalWrite(MoteurGaucheBackword, LOW);
-}
-void GoBackward()
-{
-  //Controlling speed (0 = off and 255 = max speed):
-  analogWrite(MoteurDroiteSpeed, 80); //ENA pin
-  analogWrite(MoteurGaucheSpeed, 80); //ENB pin
-
-  //Controlling spin direction of motors:
-  digitalWrite(MoteurDroiteForword, LOW);
-  digitalWrite(MoteurDroiteBackword, HIGH);
-
-  digitalWrite(MoteurGaucheForword, LOW);
-  digitalWrite(MoteurGaucheBackword, HIGH);
-}
-
-void STOP()
-{ 
-  //Controlling speed (0 = off and 255 = max speed):
-  analogWrite(MoteurDroiteSpeed, 0); //ENA pin
-  analogWrite(MoteurGaucheSpeed, 0); //ENB pin
-
-  //Controlling spin direction of motors:
-  digitalWrite(MoteurDroiteForword, LOW);
-  digitalWrite(MoteurDroiteBackword, LOW);
-
-  digitalWrite(MoteurGaucheForword, LOW);
-  digitalWrite(MoteurGaucheBackword, LOW);
-}
+  void TurnRight()
+  {
+    //Controlling speed (0 = off and 255 = max speed):
+    analogWrite(MoteurDroiteSpeed, 100); //ENA pin
+    analogWrite(MoteurGaucheSpeed, 100); //ENB pin
+    //Controlling spin direction of motors:
+    digitalWrite(MoteurDroiteForword, LOW);
+    digitalWrite(MoteurDroiteBackword, HIGH);
+    digitalWrite(MoteurGaucheForword, HIGH);
+    digitalWrite(MoteurGaucheBackword, LOW);
+  }
+  void GoBackward()
+  {
+    //Controlling speed (0 = off and 255 = max speed):
+    analogWrite(MoteurDroiteSpeed, 80); //ENA pin
+    analogWrite(MoteurGaucheSpeed, 80); //ENB pin
+    //Controlling spin direction of motors:
+    digitalWrite(MoteurDroiteForword, LOW);
+    digitalWrite(MoteurDroiteBackword, HIGH);
+    digitalWrite(MoteurGaucheForword, LOW);
+    digitalWrite(MoteurGaucheBackword, HIGH);
+  }
+  void STOP()
+  {
+    //Controlling speed (0 = off and 255 = max speed):
+    analogWrite(MoteurDroiteSpeed, 0); //ENA pin
+    analogWrite(MoteurGaucheSpeed, 0); //ENB pin
+    //Controlling spin direction of motors:
+    digitalWrite(MoteurDroiteForword, LOW);
+    digitalWrite(MoteurDroiteBackword, LOW);
+    digitalWrite(MoteurGaucheForword, LOW);
+    digitalWrite(MoteurGaucheBackword, LOW);
+  }
+  void LineFollower()
+  { if (digitalRead(TopSensor) == HIGH && digitalRead(MiddleRightSensor) == HIGH &&
+        digitalRead(MiddleLeftSensor) == HIGH )
+    {
+      GoForward();
+    }
+    else if (digitalRead(LeftSensor) == HIGH && digitalRead(RightSensor) == LOW) {
+      TurnLeft();
+    }
+    else if (digitalRead(MiddleRightSensor) == LOW && digitalRead(MiddleLeftSensor) == HIGH )
+    {
+      SlightlyTurnLeft();
+    }
+    else if (digitalRead(MiddleRightSensor) == HIGH && digitalRead(MiddleLeftSensor) == LOW )
+    {
+      SlightlyTurnRight();
+    }
+    else if (digitalRead(LeftSensor) == LOW && digitalRead(RightSensor) == HIGH) {
+      TurnRight();
+    }
+    else if (digitalRead(MiddleRightSensor) == LOW && digitalRead(MiddleLeftSensor) == LOW )
+    {
+      STOP();
+    }
+  }
